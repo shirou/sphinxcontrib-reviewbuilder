b9ec33fa8471f22734fc55fafcabab5d4f6ff701
shirou <shirou.faw@gmail.com>
2017-10-22 01:13:58 +0900

    bump 0.0.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = ['Sphinx>=1.2', 'setuptools']
 
 setup(
     name='sphinxcontrib-reviewbuilder',
-    version='0.0.6',
+    version='0.0.7',
     url='http://github.com/shirou/sphinxcontrib-reviewbuilder',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-reviewbuilder',
     license='LGPL',
