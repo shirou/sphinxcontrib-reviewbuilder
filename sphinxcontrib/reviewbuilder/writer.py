f2d31eb35235ebe475f3bb92196266d5e83c32aa
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-11-05 01:45:40 +0900

    Make "guess" as a default of code-block language

--- sphinxcontrib/reviewbuilder/writer.py
+++ sphinxcontrib/reviewbuilder/writer.py
@@ -243,7 +243,7 @@ class ReVIEWTranslator(TextTranslator):
         # TODO: remove highlight args
         self.new_state(0)
 
-        lang = node.get('language', '')
+        lang = node.get('language', 'guess')
 
         if lang == "bash":  # use cmd
             self.add_text('//cmd{' + self.nl)
